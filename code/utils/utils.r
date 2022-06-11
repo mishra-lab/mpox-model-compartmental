ec7932fef070fa493ddd184aa426c9cec2c16485
jessexknight <jesse.x.knight@gmail.com>
2022-06-11 10:39:58 -0400

    code: add rename.cols

--- code/utils/utils.r
+++ code/utils/utils.r
@@ -1,1 +1,12 @@
+
 ndim = function(x){ length(dim(x)) }
+
+rename.cols = function(x,...){
+  # e.g. rename.cols(X,a='apples') would rename column 'a' to 'apples'
+  map = list(...)
+  for (name in names(map)){
+    x[[map[[name]]]] = x[[name]]
+    x[[name]] = NULL
+  }
+  return(x)
+}
