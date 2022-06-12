54b11335364c734e925a742ac997fd5e05c2536a
jessexknight <jesse.x.knight@gmail.com>
2022-06-12 16:00:01 -0400

    code: add support for multi-model-run plots (ribbons)

--- code/model/plot.r
+++ code/model/plot.r
@@ -6,10 +6,32 @@ library('ggplot2')
   incidence = 'Incidence'
 )
 
-plot.out = function(out,x='t',y='value',ylabel,...){
+.aggr.quantile = function(out,x,y,ci=.9,strat=c('city','risk','health')){
+  # compute the median & ci quantiles on y within x, maintaining stratifications if available
+  qci = c(.5,ci.to.q(ci))
+  f = paste(y,'~',x,'+',paste(strat[strat %in% colnames(out)],collapse=' + '))
+  return(do.call(data.frame,aggregate(formula(f),out,quantile,qci,names=F)))
+}
+
+.aes.quantile = function(x,y,...){
+  # corresponding aes for .aggr.quantile using x, y, ymin, ymax
+  return(do.call(aes_string,list(x=x,y=paste0(y,'.1'),ymin=paste0(y,'.2'),ymax=paste0(y,'.3'),...)))
+}
+
+plot.out = function(out,x='t',y='value',ylabel,ci=.9,...){
+  # plot an output
   if (missing(ylabel)){ ylabel = .out.labs[[out$name[1]]] }
-  g = ggplot(out,aes_string(x=x,y=y,...)) + 
-    geom_line() +
+  if (length(unique(out$seed)) > 1){ # multiple model runs -> median line & quantile ribbon
+    g = ggplot(.aggr.quantile(out,x,y,ci=ci),.aes.quantile(x,y,...)) +
+      geom_line() +
+      geom_ribbon(alpha=.2,color=NA)
+  } else { # one model run -> one line
+    out.aes = list(x=x,y=y,...)
+    g = ggplot(out,do.call(aes_string,out.aes)) +
+      geom_line()
+  }
+  # clean-up, etc.
+   g = g +
     labs(x='Time (days)',y=ylabel)
   return(g)
 }
