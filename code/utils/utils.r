cf806f957c1c82bc302cd6225f8ee1c14d3d6e8a
jessexknight <jesse.x.knight@gmail.com>
2022-06-12 15:41:39 -0400

    code: add some utils

--- code/utils/utils.r
+++ code/utils/utils.r
@@ -1,3 +1,4 @@
+library('parallel')
 
 ndim = function(x){ length(dim(x)) }
 
@@ -9,4 +10,18 @@ rename.cols = function(x,...){
     x[[name]] = NULL
   }
   return(x)
+}
+
+ci.to.q = function(ci){
+  # e.g. ci=.95 -> c(.025,.975)
+  return(c((1-ci)/2,1-(1-ci)/2))
+}
+
+par.lapply = function(x,fun,export=NULL,n.cores=7,...){
+  # lapply fun on x in parallel, using n.cores, after exporting some variables
+  cl = makeCluster(n.cores)
+  clusterExport(cl,export)
+  R = parLapply(cl,x,fun,...)
+  stopCluster(cl)
+  return(R)
 }
