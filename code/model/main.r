a0df77b11a83a0dc68892e261232be9390b1ff9b
jessexknight <jesse.x.knight@gmail.com>
2022-06-14 21:05:31 -0400

    code: update working main

--- code/model/main.r
+++ code/model/main.r
@@ -4,18 +4,9 @@ source('model/system.r')
 source('model/output.r')
 source('model/plot.r')
 
-t = def.t(t1=365)
-
-# multiple runs
-P.n = def.params.n(70)
-R.n = sys.run.n(P.n,t,para=T)
-out = rbind(
-  out.fun.n(R.n,'incidence',aggr=T,rate=F,health='sus',city='A'),
-  out.fun.n(R.n,'incidence',aggr=T,rate=F,health='sus',city='B'))
-g = plot.out(out,ci=.5,color='city',fill='city'); print(g)
-
-# one run
+t = def.t(t1=120)
 P = def.params()
 R = sys.run(P,t)
-g = plot.out(out.incidence(R,health='sus',city='A',aggr=T,rate=F),color='risk'); print(g)
-g = plot.out(out.incidence(R,health='sus',city='A',aggr=F,rate=F),color='risk'); print(g)
+g = plot.out(out.prevalence(R,health='vax',aggr=F,prop=F),color='city',linetype='risk'); print(g)
+g = plot.out(out.prevalence(R,health='inf',aggr=F,prop=F),color='city',linetype='risk'); print(g)
+g = plot.out(out.incidence(R,health='sus',aggr=F),color='city',linetype='risk'); print(g)
