935d671087e02ad890dd842ce74cecededb14e1e
jessexknight <jesse.x.knight@protonmail.com>
2023-04-30 11:06:05 -0400

    code: update tikz for final paper

--- code/tikz/maketikz.sh
+++ code/tikz/maketikz.sh
@@ -1,3 +1,5 @@
 cat $1.tex > which.aux
+dst=../../out/fig/tikz
 pdflatex main ;\
-pdfcrop --margins '5 5 5 5' main.pdf ../../out/fig/tikz/$1.pdf
+pdfcrop --margins '5 5 5 5' main.pdf $dst/$1.pdf
+convert -density 600 -alpha remove -alpha off $dst/$1.pdf $dst/$1.png
